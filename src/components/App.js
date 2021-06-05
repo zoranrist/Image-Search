c8e9c3c51b78ae26f98e86a2ddb2143af1b416ff
ZORAN RISTOVSKI <zoranrist@yahoo.com>
2021-06-05 16:30:27 +0200

    SearchBar

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+const App = () => {
+    return (
+           <div className="ui container" style={{ marginTop: '10px'}}>               
+  {/* cemantic ui cdn   */}
+                <SearchBar />               
+           </div>
+           );
+};
+
+export default App;
